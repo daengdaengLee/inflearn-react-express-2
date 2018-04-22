6ad6abe0c2059581f0991426d3c25916b624aa66
Kunho Lee <gunho1020@gmail.com>
2018-04-22 20:35:40 +0900

    ENH: make Button Component

---
+++ src/components/1-atoms/Button/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick} >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+};
+
+export default Button;
