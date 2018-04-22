7c515fe433bc1709c0ee90684f45947fd7d2cec2
Kunho Lee <gunho1020@gmail.com>
2018-04-22 17:11:49 +0900

    ENH: make Row Component
    - use styled component
      - use flex-box design

---
+++ src/components/1-atoms/Row/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: ${props => {
+    switch(props.justifyContent) {
+      case 'end': {
+        return 'flex-end';
+      }
+      case 'center': {
+        return 'center';
+      }
+      case 'between': {
+        return 'space-between';
+      }
+      case 'around': {
+        return 'space-around';
+      }
+      default: {
+        return 'flex-start';
+      }
+    }
+  }};
+  align-items: ${props => {
+    switch(props.alignItems) {
+      case 'start': {
+        return 'flex-start';
+      }
+      case 'end': {
+        return 'flex-end';
+      }
+      case 'center': {
+        return 'center';
+      }
+      case 'baseline': {
+        return 'baseline';
+      }
+      default: {
+        return 'stretch';
+      }
+    }
+  }};
+`;
+
+const Row = ({ children, justifyContent, alignItems }) => {
+  return (
+    <FlexRow
+      justifyContent={justifyContent}
+      alignItems={alignItems} >
+      {children}
+    </FlexRow>
+  );
+}
+
+Row.propTypes = {
+  children: PropTypes.node,
+  justifyContent: PropTypes.oneOf(['start', 'end', 'center', 'between', 'around']),
+  alignItems: PropTypes.oneOf(['start', 'end', 'center', 'baseline', 'stretch']),
+};
+
+export default Row;
